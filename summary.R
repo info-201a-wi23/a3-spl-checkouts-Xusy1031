b43691d6f2c2e3b8b28f86fd16f1cf40806dc2f9
Xusy1031 <sxu7@uw.edu>
2023-02-21 20:37:50 -0800

    upload the dataset section

---
+++ summary.R
@@ -0,0 +1,8 @@
+# Load the packages
+library("dplyr")
+library("tidyverse")
+library("stringr")
+
+# Load the dataset
+df <- read.csv("~/Desktop/2022-2023-All-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
+
