d21b10ab04e50e985dad963225dd5d3cc2b4c8f4
Xusy1031 <sxu7@uw.edu>
2023-02-22 14:12:19 -0800

    finish the summary part

--- summary.R
+++ summary.R
@@ -6,7 +6,7 @@ library("stringr")
 # Load the dataset
 df <- read.csv("~/Desktop/2022-2023-All-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
 
-# What is the month with the most checkouts for ebooks?
+# What is the month with the most checkouts for ebooks from 2022 to 2023?
 most_ebooks_month <- df %>% 
   filter(MaterialType == "EBOOK") %>% 
   group_by(CheckoutMonth) %>% 
@@ -14,10 +14,10 @@ most_ebooks_month <- df %>%
   filter(most == max(most)) %>% 
   pull(CheckoutMonth)
 
-# What is the average number of checkouts for each usage class each month?
+# What is the average number of checkouts for each usage class each month from 2022 to 2023?
 avg_checkouts <- df %>% 
   group_by(UsageClass) %>% 
-  summarize(avg = mean(Checkouts)) %>% 
+  summarize(avg = (sum(Checkouts) / 13)) %>% 
   pull(avg)
 avg_checkouts <- round(avg_checkouts, digits = 0)
 
@@ -32,6 +32,13 @@ most_frequent_material_df <- df %>%
   group_by(MaterialType) %>% 
   count() %>% 
   arrange(-n)
-most_frequent_material <- most_frequent_material_df[1, ]
+most_frequent_material <- as.vector(t(most_frequent_material_df[1, ]))
+most_frequent_material
 
-
+# Which item has the highest number of checkouts in January 2023?
+highest_item <- df %>% 
+  filter(CheckoutYear == "2023") %>% 
+  filter(CheckoutMonth == "1") %>% 
+  filter(Checkouts == max(Checkouts)) %>% 
+  pull(Title)
+  
